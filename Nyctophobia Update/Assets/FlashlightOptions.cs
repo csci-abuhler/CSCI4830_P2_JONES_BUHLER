64135d2f57af50e11328d404223e6df164953611
x <x>
2019-03-25 19:17:35 -0400

    Movement update
    Player will not fly away with flashlight now

---
+++ Nyctophobia Update/Assets/FlashlightOptions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class FlashlightOptions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		GameObject light = GameObject.Find("Flashlight");
+		//Debug.Log(light.GetComponent("WhiteLight"));
+
+    }
+}
