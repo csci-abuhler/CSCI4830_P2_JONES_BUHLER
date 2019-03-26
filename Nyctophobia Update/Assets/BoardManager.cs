41d8e4f96ad5aa374bf1e8c69092d3beb23d7f8d
x <x>
2019-03-25 21:37:05 -0400

    Jones: Adding level transitioning

---
+++ Nyctophobia Update/Assets/BoardManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour
+{
+	[Serializable]
+	public class Count
+	{
+		public int minimum;
+		public int maximum;
+
+		public Count (int min, int max)
+		{
+			minimum = min;
+			maximum = max;
+		}
+	}
+
+	//public int columns = 8;
+	//public int rows = 8;
+
+	public GameObject exit;
+
+	private Transform boardHolder;
+
+	void BoardSetup()
+	{
+		boardHolder = new GameObject("Board").transform;
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Nyctophobia Update/Assets/GameManager.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+	public float levelStartDelay = 2f;
+
+
+	private float timeToComplete = 1f;
+	private Text levelText;
+	private GameObject levelImage;
+	private int level = 0;
+	private bool doingSetup;
+
+
+	private void Awake()
+	{
+		
+	}
+	
+	void OnLevelWasLoaded(int index)
+	{
+		level++;
+
+		InitGame();
+	}
+
+	void InitGame()
+	{
+		doingSetup = true;
+
+		levelImage = GameObject.Find("LevelImage");
+
+		levelText = GameObject.Find("LevelText").GetComponent<Text>();		
+
+		if(level == 0)
+		{
+			levelText.text = "Tutorial";
+
+			levelImage.SetActive(true);
+
+			Invoke("HideLevelImage", levelStartDelay);
+
+			SceneManager.LoadScene("Tutorial");
+		}
+		else if(level == 1)
+		{
+			levelText.text = "Level " + level;
+
+			levelImage.SetActive(true);
+
+			Invoke("HideLevelImage", levelStartDelay);
+
+			SceneManager.LoadScene("Level1");
+		}
+		else if(level == 2)
+		{
+			levelText.text = "Level " + level;
+
+			levelImage.SetActive(true);
+
+			Invoke("HideLevelImage", levelStartDelay);
+
+			SceneManager.LoadScene("Level2");
+		}
+		else if(level == 3)
+		{
+			levelText.text = "Level " + level;
+
+			levelImage.SetActive(true);
+
+			Invoke("HideLevelImage", levelStartDelay);
+
+			SceneManager.LoadScene("Level3");
+		}
+		else
+		{
+			GameOver();
+		}
+	}
+
+
+	void HideLevelImage()
+	{
+		levelImage.SetActive(false);
+
+		doingSetup = false;
+	}
+
+	void GameOver()
+	{
+		levelText.text = "It took you " + timeToComplete + "minutes to complete";
+		levelImage.SetActive(true);
+		enabled = false;
+	}
+
+
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		InitGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (doingSetup)
+		{
+			return;
+		}
+    }
+}
